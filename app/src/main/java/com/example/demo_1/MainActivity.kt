d41dc4a80f34fe55692eb48bc8f52da82f3b17b6
paras arora <parasarora1224@gmail.com>
2022-12-23 10:27:58 +0530

    android basics
    view , intents, passing agrument

--- app/src/main/java/com/example/demo_1/MainActivity.kt
+++ app/src/main/java/com/example/demo_1/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.demo_1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+//import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val greetingTextView = findViewById<TextView>(R.id.txt_1)
+        val editingTextView = findViewById<EditText>(R.id.edit_txt)
+        val actionButtonView = findViewById<Button>(R.id.submit_button)
+        val viewOfferButton = findViewById<Button>(R.id.id_offer)
+        var textIs = ""
+        actionButtonView.setOnClickListener {
+
+            if (editingTextView.text.toString().isNotEmpty()) {
+                textIs = editingTextView.text.toString()
+                greetingTextView.text = "Welcome ${editingTextView.text.toString()}"
+                editingTextView.text.clear()
+                viewOfferButton.setVisibility(View.VISIBLE);
+            } else {
+                viewOfferButton.setVisibility(View.GONE);
+                Toast.makeText(this@MainActivity, "Please enter your name", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+        viewOfferButton.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("keyUser" ,textIs )
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/demo_1/SecondActivity.kt
+++ app/src/main/java/com/example/demo_1/SecondActivity.kt
@@ -2,10 +2,21 @@ package com.example.demo_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("MyTag" , "Activity On create")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+
+        var textis = intent.getStringExtra("keyUser")
+        var message = "$textis, You will get access for 1 month"
+        val textView1: TextView = findViewById<TextView>(R.id.tvOffer) as TextView
+        textView1.text = message
+        Log.i("MyTag" , "message has been showed")
+//    textOffer.text = message
     }
 }
